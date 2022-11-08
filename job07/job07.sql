b09c0c01cbede2e692aa501e05ef52b17c195936
marlon-lecordier <117651017+marlon-lecordier@users.noreply.github.com>
2022-11-08 12:00:52 +0100

    Etudiants qui ont plus de 18 ans

--- job07/job07.sql
+++ job07/job07.sql
@@ -1,1 +1,1 @@
-SELECT * FROM etudiants WHERE (YEAR(CURRENT_DATE) - YEAR(naissance) - (RIGHT(CURRENT_DATE, 5) < (RIGHT(naissance, 5)))) >=18; 
+SELECT * FROM etudiants WHERE (YEAR(CURRENT_DATE) - YEAR(naissance) - (RIGHT(CURRENT_DATE, 5) < (RIGHT(naissance, 5)))) > 18; 
